7f558dc45c0f14379d613f2257c2aba5cbc081a1
Reydner Barros <reydner.alves@dcx.ufpb.br>
2026-07-24 19:37:37 -0300

    Cliente and Filme is born, i think

--- src/main/java/cinevento/irl/Filme.java
+++ src/main/java/cinevento/irl/Filme.java
@@ -1,8 +1,57 @@
 package cinevento.irl;
 
 public class Filme {
-    private String nome;
+    private int id; //id interno do filme
+    private String titulo;
     private String genero;
     private int duracao; // em segundos
-    private String especial;
+    private String classificacaoIndicativa; //L, 10, 12, 16, 18
+
+    public Filme(int id, String titulo, String genero, int duracao, String classificacaoIndicativa){
+        this.id = id;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracao = duracao;
+        this.classificacaoIndicativa = classificacaoIndicativa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setNome(String nome) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getClassificacaoIndicativa() {
+        return classificacaoIndicativa;
+    }
+
+    public void setClassificacaoIndicativa(String classificacaoIndicativa) {
+        this.classificacaoIndicativa = classificacaoIndicativa;
+    }
 }
