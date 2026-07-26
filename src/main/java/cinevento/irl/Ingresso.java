61e668c4251ef11c57db5be627d3d2aa5337e0c6
maria <maria.chagas@dcx.ufpb.br>
2026-07-26 13:28:22 -0300

    Implementação de class Ingresso

--- src/main/java/cinevento/irl/Ingresso.java
+++ src/main/java/cinevento/irl/Ingresso.java
@@ -1,6 +1,28 @@
 package cinevento.irl;
+import java.io.Serializable;
 
-public class Ingresso {
+public class Ingresso implements Serializable{
+
     private String codigoReserva;
     private Sessao sessao;
+
+    public Ingresso(String codigoReserva, Sessao sessao){
+        this.codigoReserva= codigoReserva;
+        this.sessao = sessao;
+    }
+    public String getCodigoReserva(){
+        return this.codigoReserva = codigoReserva;
+    }
+    public Sessao getSessao(){
+        return this.sessao;
+    }
+    public void setSessao(Sessao sessao){
+        this.sessao = sessao;
+    }
+    public String toString(){
+        return "Ingresso{"+
+                "codigoReserva='"+ codigoReserva+'\''+
+                ", sessao=" + sessao +
+                '}';
+    }
 }
