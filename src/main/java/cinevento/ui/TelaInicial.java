c9a5a161cd500858eb72f812fbec235b554f6e0c
Reydner Barros <reydner.alves@dcx.ufpb.br>
2026-07-27 09:25:10 -0300

    mais atualizações

---
+++ src/main/java/cinevento/ui/TelaInicial.java
@@ -0,0 +1,12 @@
+package cinevento.ui;
+
+import javax.swing.*;
+
+public class TelaInicial extends JFrame {
+    JLabel linha1, linha2, linha3;
+    JMenuBar barraDeMenu = new JMenuBar();
+
+    public TelaInicial(){
+
+    }
+}
